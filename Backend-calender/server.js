fcf65391c3a0924b61a71615fdc204dfb9e35283
darshh399 <sudarshanshinde399@gmail.com>
2025-04-05 15:50:29 +0530

    change port

--- Backend-calender/server.js
+++ Backend-calender/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const eventRouter = require("./routes/eventRoutes");
 
 dotenv.config();
 connectDB();
@@ -10,9 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/events", require("./routes/eventRoutes"));
+app.use("/api/events", eventRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
